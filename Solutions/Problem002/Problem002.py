9e61d50845a4a9067f0cbbc6d3f857c84f70047e
ian <witham.ian@gmail.com>
2022-06-26 14:59:30 +1200

    python type annotations

--- Solutions/Problem002/Problem002.py
+++ Solutions/Problem002/Problem002.py
@@ -1,15 +1,15 @@
 from itertools import takewhile
-import itertools
+from typing import Iterator
 
 
-def fibs():
+def fibs() -> Iterator[(int, int)]:
     (x, y) = (1, 2)
     while(True):
         yield x
         (x, y) = (y, x+y)
 
 
-even_fibs = filter(lambda x: x % 2 == 0, fibs())
+even_fibs: Iterator[int] = filter(lambda x: x % 2 == 0, fibs())
 print(
     sum(takewhile(lambda x: x < 4000000, even_fibs))
 )
--- Solutions/Problem003/Problem003.py
+++ Solutions/Problem003/Problem003.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import math
+from typing import Iterator
 
 
-def primefactors(x):
-    n = 2
+def primefactors(x: int) -> Iterator[int]:
+    n: int = 2
     while(n <= x):
         if (x % n == 0):
             yield n
